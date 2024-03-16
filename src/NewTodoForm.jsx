e494e2783bd4425a044eeda32dfc932987747722
Tharshan <tharshan1465@gmail.com>
2024-03-16 00:34:57 +0000

    NewToDoForm

---
+++ src/NewTodoForm.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+export function NewTodoForm(props) {
+    const [newItem, setNewItem] = useState("")
+    const handleSubmit =(event) => {
+        event.preventDefault()
+        if (newItem === "") return
+        props.addToDo(newItem)
+        setNewItem("")
+      }
+
+    return (
+        <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input value={newItem} onChange={event => setNewItem(event.target.value)} type="text" id="item"/>
+        </div>
+        <button className="btn">Add</button>
+      </form>
+    )
+}
